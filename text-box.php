4dc775c385bd41df732f313bd7af56ba4fcc68e2
yos <yos.c.bd@gmail.com>
2022-06-08 11:47:06 +0300

    second commit

--- text-box.php
+++ text-box.php
@@ -5,10 +5,10 @@
  * Requires at least: 5.7
  * Requires PHP:      7.0
  * Version:           0.1.0
- * Author:            Ali Alaa
+ * Author:           YBD
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       text-box
+ * Text Domain:       text-box1
  *
  * @package           blocks-course
  */
